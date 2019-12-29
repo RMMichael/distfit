c79650b9e49c2667f09010f68306a84944d8e8a6
Rick Michael <rickmikegull@icloud.com>
2019-12-28 20:04:43 -0800

    cycle text class

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CycleNames/>
     </div>
   );
 }
+
+//takes array of strings and cycles them on timer
+class CycleNames extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {ind: 0, names: ["Exponential","Uniform",'"Normal"'] };
+  }
+  componentDidMount() {
+    this.timerID = setInterval(()=>this.update(), 5000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+  update() {
+    this.setState((state, props) => ({
+      ind: (state.ind + 1) % this.state.names.length
+    }));
+  }
+
+  render(){
+    return(
+        <ReactCSSTransitionGroup>
+          <div>
+            <h1>{this.state.names[this.state.ind]}</h1>
+          </div>
+        </ReactCSSTransitionGroup>
+    )
+  }
+}
+
 
 export default App;
